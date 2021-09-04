0430f803bd8170885f5e93d35b314cf100594ac2
Shazalia <Shazalia>
2021-09-04 14:37:47 +0200

    initial creation

---
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.composelearning
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelearning.ui.theme.ComposeLearningTheme
+import com.example.composelearning.ui.theme.Teal200f
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearningTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("shaza")
+                }
+            }
+        }
+    }
+}
+@Preview()
+@Composable
+fun Greeting(name: String = "shaza") {
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Teal200f
+        )
+    ){
+
+        Text(text =   "Hello $name!")
+        Text(text =   "Hello $name!")
+        Text(text =   "Hello $name!")
+        Text(text =   "Hello $name!")
+    }
+}
